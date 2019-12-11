4a22c3bedf7eea97ff1638bf9affe6cd0b399ff9
mihemihe <mihemihe@hotmail.com>
2019-12-11 10:11:01 +0400

    next

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("goodbye");
             Console.WriteLine("anotherline");
+            CharEnumerator;
         }
     }
 }
