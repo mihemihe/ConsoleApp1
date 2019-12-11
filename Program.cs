d853460a7af4eccf6883c4ff3602f221a5939bdf
mihemihe <mihemihe@hotmail.com>
2019-12-11 09:43:35 +0400

    Update Program.cs
    Fix

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("goodbye");
-            Console.WriteLine("anotherline);
+            Console.WriteLine("anotherline");
         }
     }
 }
